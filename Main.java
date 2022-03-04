70f89e59e4ba2f33043d9704de72bc9a2ddf33b9
arosard <alexandre@rosard.info>
2022-03-04 17:35:50 +0100

    adding gamePanel to Main

--- Main.java
+++ Main.java
@@ -8,7 +8,9 @@ public class Main{
         window.setResizable(false);
         window.setTitle("2D Adventure");
 
-        GamePanel gamePanel = new GamePanel
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();//size the window to the preferred size and layouts of itss components
 
         window.setLocationRelativeTo(null); //to be displayed at the center of the screen
         window.setVisible(true);

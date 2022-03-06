5a9dc6ef85cd21156391a68ad17c68bd21e51c4a
arosard <alexandre@rosard.info>
2022-03-06 21:07:15 +0100

    Panel of our game after Part 2 of the turorial

--- GamePanel.java
+++ GamePanel.java
@@ -16,6 +16,9 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWindth = tileSize*maxScreenCol;//768 pix
     final int screenHeight = tileSize*maxScreenRow;//576 pix
 
+    //frames per second
+    int FPS = 60;
+
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
 
@@ -35,21 +38,70 @@ public class GamePanel extends JPanel implements Runnable{
         gameThread = new Thread(this);
         gameThread.start();
     }
-
+    /*
     @Override
     public void run() {
+        double drawInterval= 1000000000/FPS; //time between two frames in nanoseconds
+        double nextDrawTime = System.nanoTime() + drawInterval;
         while(gameThread!=null){
-            //1 update informations
             update();
-            //2 draw the updated informations
             repaint();
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime/=1000000;
+                if(remainingTime<0){
+                    remainingTime=0;
+                }
+                Thread.sleep((long)remainingTime);
+                nextDrawTime+=drawInterval;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }        
+    }*/
+    //delta-accumulation game loop
+    @Override
+    public void run() {
+        double drawInterval= (1000000000)/FPS; //time between two frames in nanoseconds
+        double delta = 0;        
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while(gameThread!=null){
+            currentTime = System.nanoTime();
+            delta += (currentTime-lastTime)/drawInterval;
+            timer += currentTime-lastTime;
+            if(delta>=1){
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+            if (timer>=(1000000000)){
+                System.out.println("FPS :"+drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+            lastTime=currentTime;
+        }  
     }
-
+    
+    //update informations
     public void update(){
-
+        if(keyH.upPressed==true){
+            playerY-=playerSpeed;
+        }else if(keyH.downPressed==true){
+            playerY+=playerSpeed;
+        }else if(keyH.leftPressed==true){
+            playerX-=playerSpeed;
+        }else if(keyH.rightPressed==true){
+            playerX+=playerSpeed;
+        }
     }
     //standard function in JPanel
+    //draw the updated informations
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
